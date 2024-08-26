07fd9e6fa631322beaf9508cf6ebd77e4d8066ed
rabiul155 <siyamict@gmail.com>
2024-08-26 22:15:25 +0600

    add shad cn

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default Home;
